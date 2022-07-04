4f62f746be450cb758988f1473b034d2b7a94180
Natalia Pinzon <nanuta522@gmail.com>
2022-07-04 16:58:04 +0200

    views file - new product and index routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -6,4 +6,8 @@ router.get("/", (req, res, next) => {
 
 router.use("/", require("./auth.routes"))
 
+router.use('/products/women', require('./product.routes'))
+
+router.use('/admin', require('./admin.routes'))
+
 module.exports = router
---
+++ routes/product.routes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router()
+
+const Product = require('./../models/Product.model')
+
+router.get('/', (req, res, next) => {
+
+    Product
+        .find()
+        .then(products => res.render('product/all-products-user', { products }))
+        .catch(error => next(error))
+})
+
+router.get('/:product_id', (req, res, next) => {
+
+    const { product_id } = req.params
+
+    Product
+        .findById(product_id)
+        .then(product => {
+            res.send('SOY DETALLES DE PRODUCTO')
+            res.render('product/product-details', product)
+    })
+        .catch(error => next(error))
+})
+
+module.exports = router
